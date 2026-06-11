e437bd2bf10e04ec38ad13b0ba000f3afc52abe1
ryeoryeong-hash <ryeoryeong@yonsei.ac.kr>
2026-06-11 17:45:16 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from transformers import pipeline
+
+generator = pipeline('text-generation', model='gpt2')
+result = generator("Artificial Intelligence is", max_length=30, num_return_sequences=1)
+print(result[0]['generated_text'])
